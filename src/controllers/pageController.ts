7b8ef4c7fc871a314ad293a689759478fef938f0
Matheus Amarante <matheusamarante1@gmail.com.br>
2022-11-25 01:19:15 -0300

    na aula ativando menu(dentro de header foi usado bolean false e true para ativar os menus porem vai ser usado outro jeito)

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -2,6 +2,12 @@ import { Request, Response} from 'express';
 
 export const home = (req: Request, res: Response) => {
    res.render('pages/page', {
+    menu: {
+        all: true,
+        dog: false,
+        cat: false,
+        fish: false
+    },
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
@@ -10,6 +16,12 @@ export const home = (req: Request, res: Response) => {
 }
 export const dogs = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: {
+            all: false,
+            dog: true,
+            cat: false,
+            fish: false
+        },
         banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
@@ -19,6 +31,12 @@ export const dogs = (req: Request, res: Response) => {
 
 export const cats = (req: Request, res: Response) => {
     res.render('pages/page', {
+        menu: {
+            all: false,
+            dog: false,
+            cat: true,
+            fish: false
+        },
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
@@ -28,6 +46,12 @@ export const cats = (req: Request, res: Response) => {
 export const fishes = (req: Request, res: Response) => {
    
     res.render('pages/page', {
+        menu: {
+            all: false,
+            dog: false,
+            cat: false,
+            fish: true
+        },
         banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
